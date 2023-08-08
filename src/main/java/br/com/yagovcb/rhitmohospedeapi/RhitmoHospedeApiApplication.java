c10d153c63e8bbf8705300dc3feb5e36ccd1f8e4
Yago Castelo Branco <yago.vcb@hotmail.com>
2023-08-08 18:16:52 -0300

    Initial commit

---
+++ src/main/java/br/com/yagovcb/rhitmohospedeapi/RhitmoHospedeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.yagovcb.rhitmohospedeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhitmoHospedeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhitmoHospedeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/yagovcb/rhitmohospedeapi/RhitmoHospedeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yagovcb.rhitmohospedeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhitmoHospedeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
